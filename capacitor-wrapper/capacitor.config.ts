704f4998b9ff54650d585df1fbd55ac253250c01
Keane1406 <keanelafourcade@gmail.com>
2026-09-12 14:58:13 +1000

    Align Android branding with Fini Species Database

--- capacitor-wrapper/capacitor.config.ts
+++ capacitor-wrapper/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.raimatak.speciesdb',
-    appName: 'Species Database App',
+    appName: 'Fini Species Database',
     webDir: '../Frontend',
 
 
