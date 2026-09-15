8d7f4980e82ce7bdb3aa413360ebdde54b6cff4e
JayCorp97 <72541142+JayCorp97@users.noreply.github.com>
2026-09-15 20:16:38 +1000

    Add translations for 'no rows to display' and improve admin token handling

--- ReactDash/src/translations.ts
+++ ReactDash/src/translations.ts
@@ -1030,6 +1030,12 @@ export const translations = {
   tet: "Iha problema. Favor tenta fali.",
   },
 
+  noRows: {
+  en: "No rows to display",
+  tet: "Laiha liña sira atu hatudu",
+  },
+
+
   pleaseFillInFields: {
   en: "Please fill in",
   tet: "Favor kompleta",
